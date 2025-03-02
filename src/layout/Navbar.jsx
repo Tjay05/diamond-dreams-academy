ac036b98a551cffc1e4d79c8b95fb3f76be4c653
Tjay05 <tjayolatayo@gmail.com>
2025-03-02 18:23:37 +0100

    Add desktop styles to navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
+      setIsMobile(window.innerWidth <= 767);
     };
     handleResize();
     window.addEventListener('resize', handleResize);
@@ -21,42 +21,57 @@ const Navbar = () => {
   const handleNavToggle = () => setToggleNav(!toggleNav);
 
   return (
-    <>
-      <div>
-        <nav className="bg-[#5c0002]">
-          <div className="flex border-b-1 justify-between items-center container py-2">
-            <div className="flex items-center gap-1">
-              <img src={Logo} className='w-10' alt="Logo" />
-              <p className="text-white font-[Rokkitt]">Diamonddreams Event</p>
-            </div>
-            {isMobile && (<img 
-              className="max-w-full" 
-              src={Hamburger} alt=""
-              onClick={handleNavToggle} 
-            />)}
+    <div>
+      <nav className="bg-[#5c0002]">
+        <div className="flex border-b-1 justify-between items-center rule py-2">
+          <div className="flex items-center gap-1">
+            <img src={Logo} className='w-10 md:w-12' alt="Logo" />
+            <p className="text-white md:text-lg font-[Rokkitt]">Diamonddreams Event</p>
           </div>
-          {isMobile && (
-            <ul className={`container py-2 flex flex-col justify-center text-white items-center font-[Open_Sans] h-h-full ${toggleNav ? '' : 'hidden'}`} >
-              <li className="nav_link">
-                <a className="links" href="/">Home</a>
-              </li>
-              <li className="nav_link">
-                <a className="links" href="/a">About me</a>
-              </li>
-              <li className="nav_link">
-                <a className="links" href="/b">Courses</a>
-              </li>
-              <li className="nav_link">
-                <a className="links" href="/v">Contact</a>
-              </li>
-              <li className="nav_link">
-                <a className="links" href="/g">Student Portal</a>
-              </li>
-            </ul>
-          )}
-        </nav>
-      </div>
-    </>
+          {isMobile && (<img 
+            className="max-w-full" 
+            src={Hamburger} alt=""
+            onClick={handleNavToggle} 
+          />)}
+          <ul className="hidden text-white items-center justify-between font-[Open_Sans] w-1/2 py-2 md:flex lg:text-lg xl:text-xl">
+            <li className="nav_link">
+              <a className="links" href="/">Home</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/a">About me</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/b">Courses</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/v">Contact</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/g">Student Portal</a>
+            </li>
+          </ul>
+        </div>
+        {isMobile && (
+          <ul className={`rule py-2 flex flex-col justify-center text-white items-center font-[Open_Sans] h-h-full ${toggleNav ? '' : 'hidden'}`} >
+            <li className="nav_link">
+              <a className="links" href="/">Home</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/a">About me</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/b">Courses</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/v">Contact</a>
+            </li>
+            <li className="nav_link">
+              <a className="links" href="/g">Student Portal</a>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </div>
   );
 }
  
