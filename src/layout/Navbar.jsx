9ce795422d0bee0f7cc76e4a2d5127ed7873e052
Tjay05 <tjayolatayo@gmail.com>
2025-03-03 21:54:55 +0100

    Add transition style to nav

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -22,17 +22,18 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="bg-[#5c0002]">
-        <div className="flex border-b-1 justify-between items-center rule py-2">
+      <nav className="bg-[#5c0002] relative">
+        <div className="flex justify-between items-center rule py-2">
           <div className="flex items-center gap-1">
             <img src={Logo} className='w-10 md:w-12' alt="Logo" />
-            <p className="text-white md:text-lg font-[Rokkitt]">Diamonddreams Event</p>
+            <p className="text-white md:text-lg font-[Rokkitt]">Diamonddreams Academy</p>
           </div>
           {isMobile && (<img 
             className="max-w-full" 
             src={Hamburger} alt=""
             onClick={handleNavToggle} 
           />)}
+          {/* Desktop Nav */}
           <ul className="hidden text-white items-center justify-between font-[Open_Sans] w-1/2 py-2 md:flex lg:text-lg xl:text-xl">
             <li className="nav_link">
               <a className="links" href="/">Home</a>
@@ -51,8 +52,9 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        {/* Mobile DropDown Nav */}
         {isMobile && (
-          <ul className={`rule py-2 flex flex-col justify-center text-white items-center font-[Open_Sans] h-h-full ${toggleNav ? '' : 'hidden'}`} >
+          <ul className={`${toggleNav ? 'opacity-100 translate-y-.5' : 'opacity-0 translate-y-full'} transition-all duration-300 shadow-md ease-in-out absolute top-13 w-full bg-inherit py-2 border-black border-t flex flex-col justify-center text-white items-center font-[Open_Sans]`} >
             <li className="nav_link">
               <a className="links" href="/">Home</a>
             </li>
