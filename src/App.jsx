832eba90156c419aef3f78f10c63fc82da9135eb
Tjay05 <tjayolatayo@gmail.com>
2025-03-06 12:08:09 +0100

    Add router to webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
 import Footer from "./layout/Footer"
+import AppLayout from './layout/Index';
 import Navbar from "./layout/Navbar"
+import Contact from "./pages/contact/Index"
+import Courses from "./pages/course/Index"
 import Home from "./pages/home/Index"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route element={<AppLayout/>}>
+        <Route index element={<Home/>}/>
+        <Route path='Courses' element={<Courses/>}/>
+        <Route path='Conatct' element={<Contact/>}/>
+      </Route>
+    </Route>
+  )
+)
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <RouterProvider router={router}/>
     </>
   )
 }
