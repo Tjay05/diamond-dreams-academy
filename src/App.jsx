ad70709d54c08246bacc6cccc3e66fc90c10b731
Tjay05 <tjayolatayo@gmail.com>
2025-03-06 13:20:07 +0100

    Link router to portal

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import {
   RouterProvider
 } from 'react-router-dom';
 
-import Footer from "./layout/Footer"
 import AppLayout from './layout/Index';
-import Navbar from "./layout/Navbar"
+import AboutMe from './pages/about/Index';
 import Contact from "./pages/contact/Index"
 import Courses from "./pages/course/Index"
 import Home from "./pages/home/Index"
+import Login from './pages/student/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route element={<AppLayout/>}>
         <Route index element={<Home/>}/>
+        <Route path='About-me' element={<AboutMe/>}/>
         <Route path='Courses' element={<Courses/>}/>
         <Route path='Contact' element={<Contact/>}/>
+        <Route path='Portal' element={<Login/>}/>
       </Route>
     </Route>
   )
