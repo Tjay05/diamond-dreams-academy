e7b5c01abb5dc17445c886ec6cfeffac96fab0ee
Tjay05 <tjayolatayo@gmail.com>
2025-03-27 23:14:21 +0100

    Comment register button out

--- src/pages/course/Index.jsx
+++ src/pages/course/Index.jsx
@@ -11,7 +11,7 @@ const Courses = () => {
         </div>
         <div className="flex flex-col items-center my-6 gap-4">
           <a href="" className="bg-[#9d58089c] py-2 px-5 text-white font-[Open_sans] text-sm rounded-md">Login</a>
-          <a href="" className="bg-[#5c0002] py-2 px-5 text-white font-[Open_sans] text-sm rounded-md">Register</a>
+          {/* <a href="" className="bg-[#5c0002] py-2 px-5 text-white font-[Open_sans] text-sm rounded-md">Register</a> */}
         </div>
       </div>
       <div className="border-2 w-full border-gray-100 rounded-md my-5">
